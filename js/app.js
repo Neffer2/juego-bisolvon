8c3dc8767761f02b2aa7e2ddff2277ae3b2614d1
Desarrollador Iglu <n3f73r@gmail.com>
2023-06-29 17:52:22 -0500

    Juego funcional (falta boton de iniciar cronometro)

--- js/app.js
+++ js/app.js
@@ -1,14 +1,35 @@
 let pulmones;
 let respiracion = true; //true inhala, false exhala 
+let virusMove = true;
+let score = 0; 
+let cronometro = 30;
+let displayScore;
+let displayCronometro;
+let mContext;
+
+let positions = Array(
+    [130, 450],
+    [160, 590],
+    [190, 520],
+
+    [220, 420],
+    [250, 555],
+    [280, 580],
+    [310, 630],
+
+    [340, 590],
+    [370, 549],
+    [400, 440],
+
+    [430, 460],
+    [460, 530],
+    [310, 540],
+    [420, 475]
+);
+let aux;
 
 // Virus
-let virus_1_1;
-let virus_1_2;
-let virus_1_3;
-let virus_2_1;
-let virus_2_2;
-let virus_3_1;
-let virus_3_2;
+let virus = [];
 
 class MainScene extends Phaser.Scene {
     constructor(){
@@ -18,64 +39,145 @@ class MainScene extends Phaser.Scene {
     preload(){ 
         this.load.image('background', './assets/img/juegobisolvon-background.jpg');
         this.load.image('pulmones', './assets/img/pulmon.png'); 
-        this.load.image('virus_1_1', './assets/img/virus_1-1.png');
-        this.load.image('virus_1_2', './assets/img/virus_1-2.png');
-        this.load.image('virus_1_3', './assets/img/virus_1-3.png');
-        this.load.image('virus_2_1', './assets/img/virus_2-1.png');
-        this.load.image('virus_2_2', './assets/img/virus_2-2.png');
-        this.load.image('virus_3_1', './assets/img/virus_3-1.png');
-        this.load.image('virus_3_2', './assets/img/virus_3-2.png');
+        this.load.image('virus1', './assets/img/virus_1.png');
+        this.load.image('virus2', './assets/img/virus_2.png');
+        this.load.image('virus3', './assets/img/virus_3.png');
+        this.load.image('virus4', './assets/img/virus_4.png');
+        this.load.image('virus5', './assets/img/virus_5.png');
+        this.load.image('virus6', './assets/img/virus_6.png');
+        this.load.image('virus7', './assets/img/virus_7.png');
         this.load.image('hero', './assets/img/hero.png');
         this.load.image('productos', './assets/img/productos.png');
         this.load.image('header', './assets/img/header.png');
         this.load.image('marcador', './assets/img/marcador.png');
+        this.load.image('cronometro', './assets/img/cronometro.png');
+        this.load.image('logo', './assets/img/logo.png'); 
     }
  
     create(){
         let background = this.add.image(257, 500, 'background').setScale(.48, .5);
+        this.getContext();
         pulmones = this.add.image(257, 500, 'pulmones').setScale(.38, .5);
         this.add.image(257, 125, 'header').setScale(.5);
         this.add.image(95, 285, 'marcador').setScale(.4);
-        
+        this.add.image(420, 285, 'cronometro').setScale(.4);
+        displayScore = this.add.text(95, 280, "0"+score, { font: '32px Courier', fill: '#ff0000' });
+        displayCronometro = this.add.text(382, 280, "0:"+cronometro, { font: '32px Courier', fill: '#ff0000' });
+
         // Virus left
-        virus_1_1 = this.add.image(150, 450, 'virus_1_1').setScale(.6);
-        virus_2_1 = this.add.image(90, 590, 'virus_2_1').setScale(.6);
-        virus_3_1 = this.add.image(200, 580, 'virus_3_1').setScale(.6);
-
-        // Virus right
-        virus_1_2 = this.add.image(310, 420, 'virus_1_2').setScale(.6);
-        virus_2_2 = this.add.image(420, 530, 'virus_2_2').setScale(.6);
-        virus_3_2 = this.add.image(310, 580, 'virus_3_2').setScale(.6);
-        virus_1_3 = this.add.image(420, 630, 'virus_1_3').setScale(.6);
-
-        virus_1_1.setInteractive()
-
-        
-        let cont = 1;
-        while(cont < 7){
-            virus_1_1
-        }
-        virus_1_1.on('pointerdown', function (pointer)
-        {   
-            virus_1_1.setTexture('hero');
-        });
+        this.generateVirus();
+        this.startCronometro();        
 
         this.add.image(257, 750, 'hero').setScale(.5);
-        this.add.image(257, 940, 'productos').setScale(.5);
+        this.add.image(257, 940, 'productos').setScale(.5);        
+    }    
+
+    generateVirus(){
+        // Destruye los elementos y vacía el arreglo
+        virus.forEach((elem) => {
+            elem[0].destroy();
+        });
+        virus = [];
+
+
+        let cont = 1;
+        let acum = 0;     
+        while(cont < 8){
+            if (cont == 1 || cont == 2 || cont == 3){
+                virus.push([this.add.image(positions[this.getRandomInt(positions.length)][0], positions[this.getRandomInt(positions.length)][1], `virus${cont}`).setScale(.6), 1]);
+            }else if (cont == 4 || cont == 5) {
+                virus.push([this.add.image(positions[this.getRandomInt(positions.length)][0], positions[this.getRandomInt(positions.length)][1], `virus${cont}`).setScale(.6), 2]);
+            }else {
+                virus.push([this.add.image(positions[this.getRandomInt(positions.length)][0], positions[this.getRandomInt(positions.length)][1], `virus${cont}`).setScale(.6), 3]);
+            }
+            cont++;
+        }
+        
+        virus.forEach((elem) => {
+            elem[0].setInteractive();
+            elem[0].on('pointerdown', function (pointer){   
+                elem[0].setTexture('logo');
+                mContext.addScore(elem[1]);
+                elem[0].disableInteractive();
+
+                acum++;
+                if (acum == 7){
+                    acum = 0;
+                    mContext.generateVirus();
+                }
+            });
+        });
     }
-    
+
+    addScore(num){
+        score += num;
+        if (score < 10){
+            displayScore.setText("0"+score);
+        }else {
+            displayScore.setText(score);
+        }
+    }
+
+    startCronometro(){
+        const interval = setInterval(() => {
+            cronometro--;
+            displayCronometro.setText("0:"+cronometro);        
+            if (cronometro == 0){ 
+                clearInterval(interval);
+                virus.forEach((elem) => {
+                    elem[0].disableInteractive();
+                });
+            }
+        }, 1000);        
+    }
+
+    getRandomInt(max) {
+        if (aux != Math.floor(Math.random() * max)){
+            aux = Math.floor(Math.random() * max)
+        }else {
+            this.getRandomInt(positions.length);
+        }
+        return aux;
+    }
+
+    getRandomDouble(max){
+        return Math.random() * max;
+    }
+
+    getContext (){
+        mContext = this;
+    }
+
     update(){
         if (respiracion){
-            pulmones.scale += .0028;
+            pulmones.scale += .0013;
             respiracion = pulmones.scale >= .6 ? !respiracion : respiracion;
         }else if (!respiracion) {
-            pulmones.scale -= .0028;
+            pulmones.scale -= .0013;
             respiracion = pulmones.scale <= .5 ? !respiracion : respiracion;            
-        }
+        }        
 
-        console.log(pulmones.scale);
+
+        if (virusMove){
+            // movimiento virus
+            virus.forEach((elem) => {
+                elem[0].y += .1
+                elem[0].x += .1
+                elem[0].angle += .5;
+
+                virusMove = elem[0].angle >= 10 ? !virusMove : virusMove;            
+            });
+        }else if (!virusMove){
+            virus.forEach((elem) => {
+                elem[0].y -= .1
+                elem[0].x -= .1
+                elem[0].angle -= .5;
+
+                virusMove = elem[0].angle <= -10 ? !virusMove : virusMove;   
+            });
+        }
     }
-}
+} 
 
 // Configuracion general
 const config = {
